64ef3577f73e252c3309cf1a8480d62cd26a9778
angeldomp49 <angeldomp49@gmail.com>
2020-10-27 00:03:01 -0600

    fetch not works

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'whatwg-fetch';
 
 class Root extends React.Component{
+
+
+
     render(){
+        testingPromises();
         return(
             <div>
                <p>Hola mundo</p>
@@ -12,3 +17,26 @@ class Root extends React.Component{
 }
 
 window.onload = () => { ReactDOM.render(<Root/>, document.querySelector("#root")); }
+
+function testingPromises(){
+    return new Promise( (resolve, reject) => {
+        var changeResult = true;
+        return changeResult ? resolve(true) : reject(error);
+    } );
+}
+
+testingPromises()
+.then( (success) => console.log("first") )
+.catch( (error) => console.log("second") );
+
+let info;
+
+fetch("http://localhost/json/data.json", {
+    method: 'GET',
+    headers: {
+        Accept: "application/json"
+    }
+})
+.then( ( response ) => { response.text(); } )
+.then( (response) => { console.log(response); info = response; } )
+.catch( ( error ) => { console.log(error) } );
