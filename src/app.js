5010c156c801e3ab7fa755e5dfb5525f823b8bd1
angeldomp49 <angeldomp49@gmail.com>
2020-11-11 22:57:17 -0600

    some clears

--- src/app.js
+++ src/app.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 
 import DatePicker from './DatePicker';
 
-import { testCalculate } from '../test/Calculate-test';
 
 class Root extends React.Component{
     render(){
         return(
             <div>
-               <p>Hola mundo</p>
                <DatePicker/>
             </div>
         );
@@ -18,5 +16,5 @@ class Root extends React.Component{
 
 window.onload = () => { 
     ReactDOM.render(<Root/>, document.querySelector("#root")); 
-    testCalculate();
+
 }
