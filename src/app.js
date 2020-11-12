b4a252852b0dfa2d3fbdffc5dbd4b5a931038aeb
angeldomp49 <angeldomp49@gmail.com>
2020-11-11 23:47:38 -0600

    a few changes after branches rebases

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import Root from "./Root";
 
 
-class Root extends React.Component{
+class App extends React.Component{
     render(){
         return(
             <div>
